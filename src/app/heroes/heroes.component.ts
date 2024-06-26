6dcbbe7a7887315f1c20568ea4a9453caad4d4b9
Hocine-bedrouni <Hocinebedrouni80@gmail.com>
2024-06-26 22:47:41 +0200

    page 2

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {Hero} from "../hero";
+import {HEROES} from "../mock-heroes";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+
+  public heroes = HEROES;
+
+  public hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
+}
