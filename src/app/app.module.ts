94fa4fa5f283d86e151a2a9b9efd130e65869b11
hocine.bedrouni <hocine.bedrouni@cgi.com>
2024-06-27 17:22:15 +0200

    page 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
-import {NgFor} from "@angular/common"; // <-- NgModel lives here
+import {NgFor} from "@angular/common";
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,42 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from "../hero";
-import {HEROES} from "../mock-heroes";
+import {HeroService} from "../hero.service";
+import {MessageService} from "../message.service";
+//import {HEROES} from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
 
-  public heroes = HEROES;
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  public selectedHero?: Hero;
+
+  public heroes: Hero[] = [];
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+
+
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
 
   public hero: Hero = {
     id: 1,
@@ -17,11 +44,10 @@ export class HeroesComponent {
   };
 
 
-  selectedHero?: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+
+
+
 
 
 }
